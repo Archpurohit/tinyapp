549d3618a2ab36485947b3dc5367eb562fb1aa80
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2022-11-24 15:56:10 -0800

    First commit

--- express_server.js
+++ express_server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
 
+// ejs
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/set", (req, res) => {
@@ -35,6 +38,33 @@ app.get("/set", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+// delete url
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
+})
