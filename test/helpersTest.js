0e77064d75393cea94dedaccde56991970348cba
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2022-12-11 18:21:52 -0800

    bug fixes

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,29 +1,59 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('/home/labber/lighthouse/tinyapp/utils/userHelper.js');
+const { getUserByEmail, getURLsByUserId, generateRandomString } = require('/home/labber/lighthouse/tinyapp/utils/userHelper.js');
 
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "uniquepassword12124"
   },
   "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: "aDifferentuniquepass998_"
+  }
+};
+
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "https://ca.nba.com/?gr=www",
+    userID: "userRandomID"
+  },
+  "sag34X": {
+    longURL: "https://github.com/",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "https://www.udemy.com/",
+    userID: "user2RandomID"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
-    const expectedUserID = "userRandomID";
-    // Write your assert statement here
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUser = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "uniquepassword12124"
+    };
+    assert.deepEqual(user, expectedUser);
   });
 
-  it('should return undefined if the email is not valid', function() {
-    const user = getUserByEmail("user3@example.com", testUsers);
-    const expectedOutput = undefined;
-    assert.equal(user,expectedOutput);
+  it('should return undefined with an invalid email', function() {
+    const user = getUserByEmail("user100@example.com", testUsers);
+    assert.isUndefined(user);
   });
+
+  it('should return undefined if first parameter is falsy', function() {
+    const user = getUserByEmail(null, testUsers);
+    assert.isUndefined(user);
+  });
+
+  it('should return undefined if second parameter is falsy', function() {
+    const user = getUserByEmail("user@example.com", null);
+    assert.isUndefined(user);
+  });
+
 });
+
